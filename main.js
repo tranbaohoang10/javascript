b4e19ddc957efd7ffe73adaeb8350f3479325bf2
Tran Bao Hoang <23130118@st.hcmuaf.edu.vn>
2025-10-02 17:32:17 +0700

    js tren html

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-const element = document.getElementById("myButton");
-const handleClickButton = () => {
-  console.log("you click a button");
-};
-element.addEventListener("click", handleClickButton);
+const myBtnElement = document.getElementById("myButton");
+const backButtonElement = document.getElementById("backButton");
+const myTextElement = document.getElementById("myText");
+myBtnElement.addEventListener("click", () => {
+  console.log("click me");
+  myTextElement.style.color = "red";
+  myTextElement.style.backgroundColor = "green";
+  myTextElement.classList.add("bao", "hoang");
+});
+backButtonElement.addEventListener("click", () => {
+  // console.log("click me");
+  myTextElement.style.color = "black";
+  myTextElement.style.backgroundColor = "unset";
+  myTextElement.classList.remove("bao", "hoang");
+});
