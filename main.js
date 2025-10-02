3ae24f97f44f6a134a0254fe775f91e1bc544fa0
Tran Bao Hoang <23130118@st.hcmuaf.edu.vn>
2025-10-02 15:38:38 +0700

    truy cap va cap nhat thuoc tinh cho object

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-let color = [2, 4, 6, 3, 7];
-const newArray = color.filter((value, index) => {
-  return value % 2 === 1;
-});
-console.log("Array First =", color);
-console.log("New Array =", newArray);
+const sv1 = {
+  name: "Tran Bao Hoang",
+  score: 10,
+};
+console.log("SinhVien before =", sv1);
+console.log("Name =", sv1.name);
+console.log("Score =", sv1["score"]);
+// cap nhat gia tri
+sv1.phone = "0369897344";
+sv1["languages"] = "Vietnamese";
+//
+//delete
+delete sv1.phone;
+console.log("SinhVien after =", sv1);
